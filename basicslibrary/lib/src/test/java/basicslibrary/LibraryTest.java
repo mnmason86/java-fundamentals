6de0afe80e8737565d70eb3b193852b0c6d7cc5c
Mandy Mason <mnmason86@gmail.com>
2022-08-18 00:34:00 -0400

    completed weatherArray feature for lab03

--- basicslibrary/lib/src/test/java/basicslibrary/LibraryTest.java
+++ basicslibrary/lib/src/test/java/basicslibrary/LibraryTest.java
@@ -38,4 +38,16 @@ class LibraryTest {
         int[] lowestArray = Library.lowestAverage(testMatrix);
         assertEquals(lowestArray, testMatrix[2]);
     }
+    @Test void testWeatherMatrix(){
+        int[][] seattleWeatherArray = {
+                {66, 64, 58, 65, 71, 57, 60},
+                {57, 65, 65, 70, 72, 65, 51},
+                {55, 54, 60, 53, 59, 57, 61},
+                {65, 56, 55, 52, 55, 62, 57}
+        };
+        String[] tempResults = Library.tempNotSeen(seattleWeatherArray);
+        //How to test?
+    }
+
+
 }
