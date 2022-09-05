40b67c70ce706a932963f3acfc90d53c6e852b7a
Mandy Mason <102629678+mnmason86@users.noreply.github.com>
2022-09-04 22:18:13 -0400

    Create noErrors.js

---
+++ linter/app/src/main/resources/noErrors.js
@@ -0,0 +1,26 @@
+function xor(a,b) {
+  if (and(a,b))
+    return 0;
+  return or(a,b);
+}
+
+function not(a) {
+  if (a === 0)
+    return 1;
+  else
+    return 0;
+}
+
+function nand(a,b) {
+  return not(and(a, b));
+}
+
+function nor(a,b) {
+  return not(or(a,b));
+}
+
+function add1(a,b) {
+  overflow = and(a,b);
+  sum = xor(a,b);
+  return {sum, overflow}
+}
